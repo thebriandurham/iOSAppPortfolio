81fae21b3b2e5e6554726c88e155fbd20c5f7c9e
Brian Durham <brian@hackerserrant.com>
2019-04-04 09:36:03 -0400

    iOS Bootcamp Day1
    iOS Bootcamp Day1

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Brian Durham on 4/3/19.
+//  Copyright © 2019 Brian Durham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateDice()
+    }
+
+    @IBAction func rollButton(_ sender: UIButton) {
+        updateDice()
+    }
+    
+    func updateDice(){
+        randomDiceIndex1 = Int.random(in: 1 ... 6)
+        randomDiceIndex2 = Int.random(in: 1 ... 6)
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+}
+
---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Brian Durham on 4/3/19.
+//  Copyright © 2019 Brian Durham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
