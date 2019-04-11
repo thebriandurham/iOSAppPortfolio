e94b0ef989fb2cc2f55cfb0920e16ff6958863d4
Brian Durham <brian@hackerserrant.com>
2019-04-11 16:29:17 -0400

    Auto Layout Calculator App
    iOS Bootcamp Day 4
    Introduces stack view layouts
    Does not have actual functionality, purpose of this was to learn stack view use.

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Brian Durham on 4/11/19.
+//  Copyright © 2019 Brian Durham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
