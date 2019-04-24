c268396b504acad8eaa83ecfb61aa50da48d37d7
Brian Durham <brian@hackerserrant.com>
2019-04-24 16:32:59 -0400

    CoreData App Template
    Useful for copying the CoreData methods from AppDelegate without having to open or create a new test project to get the code

---
+++ coredatatest/coredatatest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coredatatest
+//
+//  Created by Brian Durham on 4/24/19.
+//  Copyright © 2019 Brian Durham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
